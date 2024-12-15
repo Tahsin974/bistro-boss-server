f12b8b79f79c7e8f8083d01b9e838f4f75cfce82
Tahsin974 <tahsinislam974@gmail.com>
2024-12-15 13:10:15 +0600

    creating cart collection api

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3lwmdbh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
 
 
 // middlewares
@@ -29,16 +28,37 @@ const client = new MongoClient(uri, {
       // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
       const database = client.db("BB_DATABASE")
-      const reviewCollection = database.collection('menu'); 
-      const reviewCollections = database.collection('review'); 
+      const menuCollection = database.collection('menu'); 
+      const reviewCollection = database.collection('review'); 
+      const cartCollection = database.collection('carts'); 
 
       app.get('/menu' , async(req,res)=>{
-        const result = await reviewCollection.find({}).toArray()
+        
+        const result = await menuCollection.find({}).toArray()
         
         res.json(result)
       })
       app.get('/review' , async(req,res)=>{
-        const result = await reviewCollections.find({}).toArray()
+        const result = await reviewCollection.find({}).toArray()
+        
+        res.json(result)
+      })
+
+      // Cart Collections
+      // Get Api
+      app.get('/carts' , async(req,res)=>{
+        const email = req.query.email;
+        console.log(email)
+        const query = {customerEmail:email}
+        const result = await cartCollection.find(query).toArray();
+        
+        res.json(result)
+      })
+
+      // Post Api
+      app.post('/carts' , async(req,res)=>{
+        const cartItem = req.body;
+        const result = await cartCollection.insertOne(cartItem)
         
         res.json(result)
       })
